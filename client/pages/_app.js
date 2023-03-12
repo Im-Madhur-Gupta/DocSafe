e99775b2f6e2c4151f93d92ec3bf19700c68f97d
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-12 15:43:58 +0530

    Thirdweb Setup

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,13 @@
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Gnosis } from "@thirdweb-dev/chains";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider activeChain={Gnosis}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
 
 export default MyApp
