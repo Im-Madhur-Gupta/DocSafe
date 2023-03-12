9ada6c122a6aa8d8adbf6e15a8d042cdc90e4068
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-12 21:59:42 +0530

    Add Link Styling

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,7 +1,11 @@
 import styles from "../styles/Sidebar.module.css";
 import Link from "next/link";
+import { useRouter } from 'next/router'
 
 export default function Sidebar() {
+    const router = useRouter();
+    const path = router.asPath.split('/').pop();
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.title}>FileShare.io</div>
@@ -13,13 +17,13 @@ export default function Sidebar() {
 					<div className={styles.bar}>.</div>
 					<div className={styles.linkHolder}>
 						<Link href="/dashboard/myFiles">
-							<div className={styles.link}>My Files</div>
+							<div className={path==='myFiles' ? styles.selectedLink : styles.link}>My Files</div>
 						</Link>
 						<Link href="/dashboard/sharedFiles">
-							<div className={styles.link}>Shared with me</div>
+							<div className={path==='sharedFiles' ? styles.selectedLink : styles.link}>Shared with me</div>
 						</Link>
 						<Link href="/dashboard/create">
-							<div className={styles.link}>Add new files</div>
+							<div className={path==='create' ? styles.selectedLink : styles.link}>Add new files</div>
 						</Link>
 					</div>
 				</div>
