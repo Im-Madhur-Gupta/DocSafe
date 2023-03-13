4a24ac41178efe8dfb70d465925f56d1156bdd44
Madhur Gupta <madhurgupta107@gmail.com>
2023-03-13 21:42:10 +0530

    Add Chakra UI

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,15 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Gnosis,ChiadoTestnet } from "@thirdweb-dev/chains";
+import { Gnosis, ChiadoTestnet } from "@thirdweb-dev/chains";
+import { ChakraProvider } from '@chakra-ui/react'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider activeChain={ChiadoTestnet}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </ThirdwebProvider>
   )
 }
