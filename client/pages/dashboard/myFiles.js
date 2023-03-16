dc3ab609d8716991eb423f905756dec1f5553580
Madhur Gupta <madhurgupta107@gmail.com>
2023-03-16 22:09:12 +0530

    Fix carousel on myFiles

--- client/pages/dashboard/myFiles.js
+++ client/pages/dashboard/myFiles.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import Layout from "../../layout/layout";
 import styles from "../../styles/MyFile.module.css";
-import { motion } from "framer-motion";
+import { animate, motion, useMotionValue, useScroll } from "framer-motion";
 
 const dummyFile = [
 	{
@@ -62,13 +62,69 @@ const dummyFile = [
 ];
 
 export default function MyFiles() {
-	const [fileList, setFileList] = useState(dummyFile);
-	const [width, setWidth] = useState(0);
-	const carousel = useRef();
+	const ref = useRef(null);
+	const [startX, setStartX] = useState(0);
+	const [scrollLeft, setScrollLeft] = useState(0);
+	const [trackMouse, setTrackMouse] = useState(false);
+	const [animationComplete, setAnimationComplete] = useState(true);
 
-	useEffect(() => {
-		setWidth(carousel.current.scrollWidth);
-	}, []);
+	const x = useMotionValue(0);
+
+	const handleMouseMove = (e) => {
+		if (!ref.current) return;
+		if (!trackMouse) return;
+
+		setAnimationComplete(false);
+
+		const xVal = e.pageX - ref.current.offsetLeft;
+		const walk = (xVal - startX) * 2; //scroll-fast
+
+		const controls = animate(x, scrollLeft - walk, {
+			type: "tween",
+			ease: "easeOut",
+			duration: 0,
+			onUpdate: (val) => {
+				if (!ref.current) return;
+				ref.current.scrollLeft = val;
+			},
+			onComplete: () => {
+				setAnimationComplete(true);
+			},
+			onStop: () => {
+				setAnimationComplete(true);
+			}
+		});
+		return controls.stop;
+	};
+
+	const handleMouseDown = (e) => {
+		// if (!(e.target instanceof HTMLLIElement)) return;
+		if (!ref.current) return;
+
+		setTrackMouse(true);
+
+		const startX = e.pageX - ref.current.offsetLeft;
+		setStartX(startX);
+
+		const scrollLeft = ref.current.scrollLeft;
+		setScrollLeft(scrollLeft);
+	};
+
+	const handleMouseLeave = () => {
+		setTrackMouse(false);
+	};
+
+	const handleMouseUp = () => {
+		setTrackMouse(false);
+	};
+
+	const handleScroll = () => {
+		if (!ref.current) return;
+
+		if (animationComplete) {
+			x.set(ref.current.scrollLeft);
+		}
+	};
 
 	return (
 		<Layout>
@@ -82,19 +138,20 @@ export default function MyFiles() {
 						<h1 className={styles.myFilesTitle}>My Files</h1>
 					</div>
 					<motion.div
-						ref={carousel}
 						className={styles.myFileHolderParent}
 						whileTap={{ cursor: "grabbing" }}
+						ref={ref}
+						onMouseMove={handleMouseMove}
+						onMouseDown={handleMouseDown}
+						onMouseUp={handleMouseUp}
+						onMouseLeave={handleMouseLeave}
+						onScroll={handleScroll}
 					>
-						<motion.div
-							drag="x"
-							dragConstraints={{ right: 0, left: -width }}
-							className={styles.myFilesHolder}
-						>
-							{fileList.map((item, index) => (
+						{dummyFile.map((item, index) => (
 								<motion.div
 									key={index}
 									className={styles.filebox}
+								style={{ userSelect: trackMouse ? "none" : "auto" }}
 								>
 									<div className={styles.fileTextHolder}>
 										<h1 className={item.filename}>
@@ -105,8 +162,7 @@ export default function MyFiles() {
 										</h3>
 									</div>
 								</motion.div>
-							))}
-						</motion.div>
+						))}
 					</motion.div>
 				</div>
 			</div>
