3e05afaabcee03539573e25966956e92d9bee892
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-16 18:25:28 +0530

    Add Framer Motion

--- client/pages/dashboard/myFiles.js
+++ client/pages/dashboard/myFiles.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Layout from "../../layout/layout";
 import styles from "../../styles/MyFile.module.css";
+import { motion } from "framer-motion";
 
 const dummyFile = [
 	{
@@ -33,12 +34,41 @@ const dummyFile = [
 		size: "1mb",
 		sender: "Jhon Doe",
 	},
-	
-
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
 ];
 
 export default function MyFiles() {
 	const [fileList, setFileList] = useState(dummyFile);
+	const [width, setWidth] = useState(0);
+	const carousel = useRef();
+
+	useEffect(() => {
+		setWidth(carousel.current.scrollWidth);
+	}, []);
 
 	return (
 		<Layout>
@@ -48,21 +78,36 @@ export default function MyFiles() {
 					<h3 className={styles.username}>@username</h3>
 				</div>
 				<div className={styles.myFilesContainer}>
-                    <div className={styles.myFilesTitleHolder}>
-
-					<h1 className={styles.myFilesTitle}>My Files</h1>
-                    </div>
-					<div className={styles.myFilesHolder}>
-						{fileList.map((item, index) => (
-							<div key={index} className={styles.filebox}>
-								<div className={styles.fileTextHolder}>
-                                    <h1 className={item.filename}>{item.filename}</h1>
-                                    <h3 className={item.filesize}>{item.size}</h3>
-                                </div>
-
-							</div>
-						))}
+					<div className={styles.myFilesTitleHolder}>
+						<h1 className={styles.myFilesTitle}>My Files</h1>
 					</div>
+					<motion.div
+						ref={carousel}
+						className={styles.myFileHolderParent}
+						whileTap={{ cursor: "grabbing" }}
+					>
+						<motion.div
+							drag="x"
+							dragConstraints={{ right: 0, left: -width }}
+							className={styles.myFilesHolder}
+						>
+							{fileList.map((item, index) => (
+								<motion.div
+									key={index}
+									className={styles.filebox}
+								>
+									<div className={styles.fileTextHolder}>
+										<h1 className={item.filename}>
+											{item.filename}
+										</h1>
+										<h3 className={item.filesize}>
+											{item.size}
+										</h3>
+									</div>
+								</motion.div>
+							))}
+						</motion.div>
+					</motion.div>
 				</div>
 			</div>
 		</Layout>
