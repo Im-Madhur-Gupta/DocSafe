6e2f34f893077e73ce59b619402bd264c8968e8e
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-19 17:51:37 +0530

    Add Share with functionality

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,17 +1,25 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Gnosis, ChiadoTestnet } from "@thirdweb-dev/chains";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import { extendTheme } from "@chakra-ui/react";
 
-import '../styles/globals.css'
+const theme = extendTheme({
+	colors: {
+		brand: {
+      100: "#4285F4",
+    },
+	},
+});
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThirdwebProvider activeChain={ChiadoTestnet}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ThirdwebProvider>
-  )
+	return (
+		<ThirdwebProvider activeChain={ChiadoTestnet}>
+			<ChakraProvider theme={theme}>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</ThirdwebProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- client/pages/dashboard/create.js
+++ client/pages/dashboard/create.js
@@ -3,10 +3,48 @@ import styles from "../../styles/Create.module.css";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
 import { useEffect, useRef, useState } from "react";
+import {
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+	FormControl,
+	FormLabel,
+	FormErrorMessage,
+	FormHelperText,
+	Input,
+	Button,
+	Stack,
+	Tag,
+	TagLabel,
+	TagCloseButton,
+	useDisclosure,
+} from "@chakra-ui/react";
 
 export default function create() {
 	const wrapperRef = useRef(null);
 	const [fileList, setFileList] = useState([]);
+	const [shareWith, setShareWith] = useState([]);
+	const { isOpen, onToggle } = useDisclosure();
+	const [tabIndex, setTabIndex] = useState(0);
+	const [fileName,setFileName]=useState("")
+
+	function handleNextTab() {
+		setTabIndex(1);
+	}
+
+	function handleKeyDown(e) {
+		const inputValue = e.target.value;
+		if (e.key === "Enter" && inputValue) {
+			setShareWith([...shareWith, inputValue]);
+			e.target.value = "";
+		}
+	}
+
+	function removeTag(tagToRemove) {
+		setShareWith(shareWith.filter((email) => email !== tagToRemove));
+	}
 
 	function onDragEnter() {
 		return wrapperRef.current.classList.add("dragover");
@@ -45,49 +83,133 @@ export default function create() {
 	return (
 		<Layout>
 			<div className={styles.container}>
-				<div className={styles.dndBox}>
-					<div
-						ref={wrapperRef}
-						className={styles.dndContainer}
-						onDragEnter={onDragEnter}
-						onDragLeave={onDragLeave}
-						onDrop={onDrop}
+				<div className={styles.tabHoldler}>
+					<Tabs
+						index={tabIndex}
+						onChange={setTabIndex}
+						variant="soft-rounded"
+						colorScheme="brand"
+						size="lg"
+						align="center"
+						isFitted
 					>
-						<div className={styles.dndLabel}>
-							<AiOutlineCloudUpload size={120} />
-							<p>Drag & Drop your files here</p>
-						</div>
-						<input
-							className={styles.dndInput}
-							type="file"
-							value=""
-							onChange={onFileDrop}
-						/>
-					</div>
-					{fileList.length > 0 && (
-						<div className={styles.previewFiles}>
-							{fileList.map((item, index) => (
-								<div key={index} className={styles.filebox}>
-									<p>{item.name}</p>
-									<p>{item.size}B</p>
-									<div className={styles.cancelHolder}>
-										<RxCross1
-											onClick={() => fileRemove(item)}
-											size={20}
+						<TabList>
+							<Tab>Enter Details</Tab>
+							<Tab>Upload Files</Tab>
+						</TabList>
+						<TabPanels>
+							<TabPanel>
+								<div className={styles.formHolder}>
+									<FormControl isRequired color="white">
+										<FormLabel>File Name</FormLabel>
+										<Input
+											placeholder="Enter the name of the file"
+											size="lg"
+											type="name"
+											value={fileName}
+											onChange={(e)=>setFileName(e.target.value)}
+										/>
+									</FormControl>
+									<FormControl isRequired color="white">
+										<FormLabel>Share with</FormLabel>
+										<Input
+											type="email"
+											placeholder="Add email of the people to share with"
+											size="lg"
+											onKeyDown={handleKeyDown}
+										/>
+									</FormControl>
+									{shareWith.length > 0 && (
+										<div className={styles.emailsHolder}>
+											{shareWith.map((email) => (
+												<Tag
+													key={email}
+													size="lg"
+													borderRadius="full"
+													variant="solid"
+													bg="brand.100"
+												>
+													<TagLabel>{email}</TagLabel>
+													<TagCloseButton
+														onClick={() => {
+															removeTag(email);
+														}}
+													/>
+												</Tag>
+											))}
+										</div>
+									)}
+									<Button
+										color="white"
+										bg="brand.100"
+										size="lg"
+										onClick={handleNextTab}
+									>
+										Continue
+									</Button>
+								</div>
+							</TabPanel>
+							<TabPanel>
+								<div className={styles.dndBox}>
+									<div
+										ref={wrapperRef}
+										className={styles.dndContainer}
+										onDragEnter={onDragEnter}
+										onDragLeave={onDragLeave}
+										onDrop={onDrop}
+									>
+										<div className={styles.dndLabel}>
+											<AiOutlineCloudUpload size={120} />
+											<p>Drag & Drop your files here</p>
+										</div>
+										<input
+											className={styles.dndInput}
+											type="file"
+											value=""
+											onChange={onFileDrop}
 										/>
 									</div>
+									{fileList.length > 0 && (
+										<div className={styles.previewFiles}>
+											{fileList.map((item, index) => (
+												<div
+													key={index}
+													className={styles.filebox}
+												>
+													<p>{item.name}</p>
+													<p>{item.size}B</p>
+													<div
+														className={
+															styles.cancelHolder
+														}
+													>
+														<RxCross1
+															onClick={() =>
+																fileRemove(item)
+															}
+															size={20}
+														/>
+													</div>
+												</div>
+											))}
+											<div
+												className={
+													styles.buttonContainer
+												}
+											>
+												<button
+													onClick={handleSubmit}
+													className={styles.uploadBtn}
+												>
+													Add Files
+												</button>
+											</div>
+										</div>
+									)}
 								</div>
-							))}
-							<div className={styles.buttonContainer}>
-								<button
-									onClick={handleSubmit}
-									className={styles.uploadBtn}
-								>
-									Add Files
-								</button>
-							</div>
-						</div>
-					)}
+							</TabPanel>
+						</TabPanels>
+					</Tabs>
 				</div>
 			</div>
 		</Layout>
