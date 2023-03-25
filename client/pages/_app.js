5778da80a8599bdcf4524f721588ed30214c50a6
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-25 11:24:33 +0530

    Add Env

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,8 +9,7 @@ import { usePolybase, useDocument } from "@polybase/react";
 import { Auth } from "@polybase/auth";
 
 const polybase = new Polybase({
-  defaultNamespace:
-    "pk/0x34d5564c5d359879ee5cad5e01f6094a4ca6f695fc9091296d8fbdf27c0d670bfffe2cd69af2fefe318cf22867c3e614dbee68caf71e3129d94a6b0f4459986b/poly-safe-app",
+  defaultNamespace:process.env.NEXT_PUBLIC_POLYBASE_NAMESPACE,
 });
 const auth = typeof window !== "undefined" ? new Auth() : null;
 
