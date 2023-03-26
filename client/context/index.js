64d409384d2530ba054737bc5df727b7253f822d
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-26 12:55:44 +0530

    Add Contract Calls

--- client/context/index.js
+++ client/context/index.js
@@ -1,105 +1,126 @@
 import { useContext, createContext } from "react";
-import {
-	useAddress,
-} from "@thirdweb-dev/react";
+import { useAddress } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 import { fileManagerAddress } from "../constants";
-import ABI from "../artifacts/contracts/FileManager.sol/FileManager.json"
-
+import ABI from "../artifacts/contracts/FileManager.sol/FileManager.json";
 
 const StateContext = createContext();
 
 export function StateContextProvider({ children }) {
+	const address = useAddress();
+	const contractAddress = fileManagerAddress;
+	const contractABI = ABI.abi;
 
-    const address = useAddress();
-    const contractAddress=fileManagerAddress;
-    const contractABI=ABI.abi
-
-    async function createSafe(name,cid,fileNames){
-        if(typeof window.ethereum!=="undefined"){
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
+	async function createSafe(name, cid, fileNames) {
+		if (typeof window.ethereum !== "undefined") {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
 			const signer = provider.getSigner();
-            const contract = new ethers.Contract(
-                contractAddress,
-                contractABI,
-                signer
-            );
-            try{
-                const res = await contract.createSafe(name,cid,fileNames);
-                await provider.waitForTransaction(res.hash);
-                console.log("Res",res);
-            }catch(err){
-                console.log("Error",err);
-            }
-        }
-    }
+			const contract = new ethers.Contract(
+				contractAddress,
+				contractABI,
+				signer
+			);
+			try {
+				const res = await contract.createSafe(name, cid, fileNames);
+				await provider.waitForTransaction(res.hash);
+				console.log("Res", res);
+			} catch (err) {
+				console.log("Error", err);
+			}
+		}
+	}
 
-    async function addAllowed(fileName,userAddress){
-        if(typeof window.ethereum!=="undefined"){
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(
-                contractAddress,contractABI,
-                signer
-            );
-            try{
-                const res = await contract.addAllowed(fileName,userAddress);
-                await provider.waitForTransaction(res.hash);
-                console.log("Res",res);
-            }catch(err){
-                console.log("Error",err);
-            }
-        }
-    }
+	async function addAllowed(fileName, userAddress) {
+		if (typeof window.ethereum !== "undefined") {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(
+				contractAddress,
+				contractABI,
+				signer
+			);
+			try {
+				const res = await contract.addAllowed(fileName, userAddress);
+				await provider.waitForTransaction(res.hash);
+				console.log("Res", res);
+			} catch (err) {
+				console.log("Error", err);
+			}
+		}
+	}
 
-    async function removeAllowed(fileName,userAddress){
-        if(typeof window.ethereum!=="undefined"){
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(
-                contractAddress,
-                contractABI,
-                signer
-            );
-            try{
-                const res = await contract.removeAllowed(fileName,userAddress);
-                await provider.waitForTransaction(res.hash);
-                console.log("Res",res);
-            }catch(err){
-                console.log("Error",err);
-            }
-        }
-    }
+	async function removeAllowed(fileName, userAddress) {
+		if (typeof window.ethereum !== "undefined") {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(
+				contractAddress,
+				contractABI,
+				signer
+			);
+			try {
+				const res = await contract.removeAllowed(fileName, userAddress);
+				await provider.waitForTransaction(res.hash);
+				console.log("Res", res);
+			} catch (err) {
+				console.log("Error", err);
+			}
+		}
+	}
 
-    async function fetchUserSafes(userAddress){
-        if(typeof window.ethereum!=="undefined"){
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(
-                contractAddress,
-                contractABI,
-                signer
-            );
-            try{
-                const res = await contract.getUserSafes()
-            }
-        }
-    }
+	async function fetchUserSafes(userAddress) {
+		let provider;
+		if (process.env.NEXT_PUBLIC_ENVIRONMENT === "local") {
+			provider = new ethers.providers.JsonRpcProvider();
+		} else if (process.env.NEXT_PUBLIC_ENVIRONMENT === "testnet") {
+			provider = new ethers.providers.JsonRpcProvider(
+				process.env.NEXT_PUBLIC_MANTLE_RPC_URL
+			);
+		}
+		const contract = new ethers.Contract(
+			contractAddress,
+			contractABI,
+			provider
+		);
+		try {
+			const res = await contract.getUserSafes(userAddress);
+			return res;
+		} catch (err) {
+			console.log("Error", err);
+		}
+	}
 
-    return (
-        <StateContext.Provider
-            value={{
-                address,
-                createSafe,
-                addAllowed,
-                removeAllowed
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
+	async function fetchSafesSharedWithUser(userAddress) {
+		let provider;
+		if (process.env.NEXT_PUBLIC_ENVIRONMENT === "local") {
+			provider = new ethers.providers.JsonRpcProvider();
+		} else if (process.env.NEXT_PUBLIC_ENVIRONMENT === "testnet") {
+			provider = new ethers.providers.JsonRpcProvider(
+				process.env.NEXT_PUBLIC_MANTLE_RPC_URL
+			);
+		}
+		try {
+			const res = await contract.getSafesSharedWith(userAddress);
+			return res;
+		} catch (err) {
+			console.log("Error", err);
+		}
+	}
 
-
+	return (
+		<StateContext.Provider
+			value={{
+				address,
+				createSafe,
+				addAllowed,
+				removeAllowed,
+				fetchUserSafes,
+				fetchSafesSharedWithUser,
+			}}
+		>
+			{children}
+		</StateContext.Provider>
+	);
 }
 
 export function useStateContext() {
