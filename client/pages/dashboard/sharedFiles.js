64a31cfd5c36577d5181a6e5d0ddfa65ee8855dc
Aditya Bhattad <adityabhattad18@gmail.com>
2023-03-26 16:34:18 +0530

    Fix Shared Files

--- client/pages/dashboard/sharedFiles.js
+++ client/pages/dashboard/sharedFiles.js
@@ -10,58 +10,42 @@ import {
 } from "@chakra-ui/react";
 import styles from "../../styles/Shared.module.css";
 import { useState } from "react";
-import { useStateContext } from "../../context";
+
+const dummyFile = [
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+	{
+		filename: "file1.xyz",
+		size: "1mb",
+		sender: "Jhon Doe",
+	},
+];
 
 export default function SharedFiles() {
-	const [fileList, setFileList] = useState([]);
-	const { address, fetchSafesSharedWithUser } = useStateContext();
-
-	useEffect(() => {
-		if (address) {
-			fetchSafesSharedWithUser(address)
-				.then((res) => {
-					setFileList(res);
-				})
-				.catch((err) => {
-					console.log("Error", err);
-				});
-		}
-	}, []);
-
-	const openPdfFileHandler = (cid, filename) => {
-		router.push(`/view-pdf/${cid}?filename=${filename}`);
-	};
-
-	const openMediaFileHandler = (cid, filename) => {
-		router.push(`/view-media/${cid}?filename=${filename}`);
-	};
-
-	const viewFileHandler = (cid, filename) => {
-		const formattedCid = cid.split("/")[0];
-		console.log("CID", formattedCid);
-
-		const formattedFilename = encodeURIComponent(filename);
-		console.log("Filename", formattedFilename);
-
-		const filetype = formattedFilename.split(".")[1].toLocaleLowerCase();
-
-		if (filetype === "pdf") {
-			return openPdfFileHandler(formattedCid, formattedFilename);
-		}
-
-		const mediaFileTypes = [
-			"mp4",
-			"jpeg",
-			"jpg",
-			"png",
-			"gif",
-			"webp",
-			"svg",
-		];
-		if (mediaFileTypes.includes(filetype)) {
-			return openMediaFileHandler(formattedCid, formattedFilename);
-		}
-	};
+	const [fileList, setFileList] = useState(dummyFile);
 
 	return (
 		<Layout>
@@ -94,28 +78,13 @@ export default function SharedFiles() {
 								</Tr>
 							</Thead>
 							<Tbody>
-								{fileList.map((item, index) => {
-									for (let x = 0; x < item[3].length; x++) {
-										return (
-											<Tr
-												key={index}
-												onClick={() =>
-													viewFileHandler(
-														item[1],
-														item[3][x]
-													)
-												}
-											>
-												<Td>{item[3][x]}</Td>
-												<Td>{item[2]}</Td>
-											</Tr>
-										);
-									}
+								{fileList.map((item, index) => (
 									<Tr key={index}>
 										<Td>{item.filename}</Td>
 										<Td>{item.sender}</Td>
-									</Tr>;
-								})}
+										<Td>{item.size}</Td>
+									</Tr>
+								))}
 							</Tbody>
 						</Table>
 					</TableContainer>
--- smart-contracts/contracts/FileManager.sol
+++ smart-contracts/contracts/FileManager.sol
@@ -1,59 +1,87 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-
 contract FileManager {
-
     struct Safe {
         string name;
         string cid;
         address owner;
+        address accessNFT;
         string[] fileNames;
     }
 
-    mapping (uint256 => Safe) private safes;
-    mapping (uint256 => mapping (address => bool)) allowed;
-    uint256 public  safeCount;
+    mapping(uint256 => Safe) private safes;
+    mapping(uint256 => mapping(address => bool)) allowed;
+    uint256 public safeCount;
 
-    function createSafe(string memory _name, string memory _cid,string[] memory fileNames) public {
+    function createSafe(
+        string memory _name,
+        string memory _cid,
+        string[] memory fileNames
+    ) public {
         safeCount++;
-        safes[safeCount] = Safe(_name, _cid, msg.sender,fileNames);
+        safes[safeCount] = Safe(_name, _cid, msg.sender, address(0), fileNames);
     }
 
     function addAllowed(string memory _name, address _allowed) public {
-        
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if (keccak256(abi.encodePacked(safes[i].name)) == keccak256(abi.encodePacked(_name))) {
-                require(msg.sender == safes[i].owner, "Only owner can add allowed");
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (
+                keccak256(abi.encodePacked(safes[i].name)) ==
+                keccak256(abi.encodePacked(_name))
+            ) {
+                require(
+                    msg.sender == safes[i].owner,
+                    "Only owner can add allowed"
+                );
                 allowed[i][_allowed] = true;
             }
         }
+    }
 
+    function addAccessNFT(string memory _name, address _accessNFT) public {
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (
+                keccak256(abi.encodePacked(safes[i].name)) ==
+                keccak256(abi.encodePacked(_name))
+            ) {
+                require(
+                    msg.sender == safes[i].owner,
+                    "Only owner can add allowed"
+                );
+                safes[i].accessNFT = _accessNFT;
+            }
+        }
     }
 
     function removeAllowed(string memory _name, address _allowed) public {
-
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if (keccak256(abi.encodePacked(safes[i].name)) == keccak256(abi.encodePacked(_name))) {
-                require(msg.sender == safes[i].owner, "Only owner can remove allowed");
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (
+                keccak256(abi.encodePacked(safes[i].name)) ==
+                keccak256(abi.encodePacked(_name))
+            ) {
+                require(
+                    msg.sender == safes[i].owner,
+                    "Only owner can remove allowed"
+                );
                 allowed[i][_allowed] = false;
             }
         }
-
     }
 
-   function getSafesSharedWith(address _user) public view returns (Safe[] memory) {
+    function getSafesSharedWith(
+        address _user
+    ) public view returns (Safe[] memory) {
         uint256 count = 0;
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if(allowed[i][_user]) {
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (allowed[i][_user]) {
                 count++;
             }
         }
 
         Safe[] memory sharedSafes = new Safe[](count);
         count = 0;
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if(allowed[i][_user]) {
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (allowed[i][_user]) {
                 sharedSafes[count] = safes[i];
                 count++;
             }
@@ -62,18 +90,18 @@ contract FileManager {
         return sharedSafes;
     }
 
-    function getUserSafes(address _user) public view returns(Safe[] memory) {
+    function getUserSafes(address _user) public view returns (Safe[] memory) {
         uint256 count = 0;
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if(safes[i].owner == _user) {
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (safes[i].owner == _user) {
                 count++;
             }
         }
 
         Safe[] memory userSafes = new Safe[](count);
         count = 0;
-        for(uint256 i = 1; i <= safeCount; i++) {
-            if(safes[i].owner == _user) {
+        for (uint256 i = 1; i <= safeCount; i++) {
+            if (safes[i].owner == _user) {
                 userSafes[count] = safes[i];
                 count++;
             }
@@ -84,14 +112,18 @@ contract FileManager {
 
     function deleteSafe(string memory _name) public {
         for (uint256 i = 1; i <= safeCount; i++) {
-            if (keccak256(abi.encodePacked(safes[i].name)) == keccak256(abi.encodePacked(_name))) {
-                require(msg.sender == safes[i].owner, "Only owner can delete safe");
+            if (
+                keccak256(abi.encodePacked(safes[i].name)) ==
+                keccak256(abi.encodePacked(_name))
+            ) {
+                require(
+                    msg.sender == safes[i].owner,
+                    "Only owner can delete safe"
+                );
                 delete safes[i];
                 safeCount--;
                 return;
             }
         }
     }
-
 }
-
